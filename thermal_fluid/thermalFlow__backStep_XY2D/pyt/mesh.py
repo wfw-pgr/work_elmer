9de4f15e5c607581037277673c6dd35a69a69ed1
wfw-pgr <wfw.pgr@gmail.com>
2023-02-10 19:10:17 +0900

    update

---
+++ thermal_fluid/thermalFlow__backStep_XY2D/pyt/mesh.py
@@ -0,0 +1,56 @@
+import numpy as np
+import os, sys
+import gmsh
+import nkGmshRoutines.define__geometry_2d as dg2
+import nkGmshRoutines.load__dimtags as ldt
+
+
+# ========================================================= #
+# ===   実行部                                          === #
+# ========================================================= #
+
+if ( __name__=="__main__" ):
+
+    # ------------------------------------------------- #
+    # --- [1] initialization of the gmsh            --- #
+    # ------------------------------------------------- #
+    gmsh.initialize()
+    gmsh.option.setNumber( "General.Terminal", 1 )
+    gmsh.option.setNumber( "Mesh.Algorithm"  , 5 )
+    gmsh.option.setNumber( "Mesh.Algorithm3D", 4 )
+    gmsh.option.setNumber( "Mesh.SubdivisionAlgorithm", 0 )
+    gmsh.model.add( "model" )
+    
+    # ------------------------------------------------- #
+    # --- [2] Modeling                              --- #
+    # ------------------------------------------------- #
+    dimtags = {}
+    inpFile = "dat/geometry.conf"
+    bdrFile = "dat/boundary.json"
+    dimtags = dg2.define__geometry_2d( inpFile=inpFile, dimtags=dimtags )
+    dimtags = ldt.load__dimtags( dimtags=dimtags, inpFile=bdrFile )
+    gmsh.model.occ.synchronize()
+
+    # ------------------------------------------------- #
+    # --- [3] Mesh settings                         --- #
+    # ------------------------------------------------- #
+    mesh_from_config = True          # from nkGMshRoutines/test/mesh.conf, phys.conf
+    uniform_size     = 0.05
+    if ( mesh_from_config ):
+        meshFile = "dat/mesh.conf"
+        physFile = "dat/phys.conf"
+        import nkGmshRoutines.assign__meshsize as ams
+        meshes = ams.assign__meshsize( meshFile=meshFile, physFile=physFile, \
+                                       dimtags=dimtags, target="surf" )
+    else:
+        import nkGmshRoutines.assign__meshsize as ams
+        meshes = ams.assign__meshsize( uniform=uniform_size, dimtags=dimtags )
+
+    # ------------------------------------------------- #
+    # --- [4] post process                          --- #
+    # ------------------------------------------------- #
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(2)
+    gmsh.write( "msh/model.msh" )
+    gmsh.finalize()
+
